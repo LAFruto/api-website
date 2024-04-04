30dbd8859df360d27a5f66addae2aac248202bd2
Luis Angelo Fruto <luisfruto7@gmail.com>
2024-04-04 21:11:35 +0800

    location section

--- components/contact/Location.tsx
+++ components/contact/Location.tsx
@@ -1,9 +1,32 @@
+import Image from "next/image";
+import MapEmbed from "./MapEmbed";
+import Banner from "../Banner";
+
 const Location = () => {
   return (
-    <section className="bg-blue-50 h-[500px]">
-      <div>
-    
+    <section className="bg-blue-50 rounded-tl-xl rounded-tr-xl">
+      <div className="max-container padding-container py-16 lg:py-24 flex flex-col gap-6 lg:flex-row">
+        <div className="flex flex-col justify-between w-full lg:w-[51%]">
+          <h1 className="text-white leading-tight bold-46 lg:bold-64">Where to Find us</h1>
+          <div className="mt-5 gap-4 flex lg:flex-col items-center lg:items-start">
+            <Image
+              src={"/images/mapua.png"}
+              alt="mmcm"
+              width={150}
+              height={150}
+              quality={100}
+            />
+            <p className="text-white">
+              Located at <strong>Mapúa Malayan Colleges Mindanao</strong>, Gen. Douglas MacArthur Hwy, Talomo, Davao City
+            </p>
+          </div>
+        </div>
+        <MapEmbed/>
       </div>
+      <Banner 
+        image={"/images/contact.jpg"}
+        title={"contact banner"}
+      />
     </section>
   );
 };
