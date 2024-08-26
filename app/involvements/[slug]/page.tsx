5a39551f435cd9b1c726a6c8934f8d06173e2e2f
Luis Angelo Fruto <luisfruto7@gmail.com>
2024-08-26 10:37:22 +0800

    perf: test

--- app/involvements/[slug]/page.tsx
+++ app/involvements/[slug]/page.tsx
@@ -1,11 +1,11 @@
 import Involvement from "@/components/involvements/Involvement";
 import { getAllContent, getContentEntry } from "@/data";
 
-export const generateStaticParams = async () => {
-  const content = await getAllContent();
+// export const generateStaticParams = async () => {
+//   const content = await getAllContent();
 
-  return content.map((post) => ({ slug: post.slug }));
-};
+//   return content.map((post) => ({ slug: post.slug }));
+// };
 
 export default async function ContentPage({ params }: { params: { slug: string } }) {
   const content = await getContentEntry(params.slug);
